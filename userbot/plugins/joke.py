21b2a925ff9dc957557579a82febc9ce98283fd5
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-08-20 15:44:57 +0530

    Update joke.py

--- userbot/plugins/joke.py
+++ userbot/plugins/joke.py
@@ -9,3 +9,19 @@ from userbot import catub
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from ..helpers.utils import reply_id
+
+plugin_category = "fun"
+
+
+@catub.cat_cmd(
+    pattern="joke(?:\s|$)([\s\S]*)",
+    command=("joke", plugin_category),
+    info={
+        "header": "Just For Fun.",
+        "description": "CMD For Get Jokes",
+        "usage": "{tr}joke",
+    },
+)
+async def jokeapi(event):
+
+
