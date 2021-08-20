01c9b6bb87367a17484301bb575df0e08360ec2e
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-08-20 13:31:05 +0530

    Create joke.py

---
+++ userbot/plugins/joke.py
@@ -0,0 +1,11 @@
+import os
+
+import requests
+
+import jokeapi
+
+from userbot import catub
+
+from ..Config import Config
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.utils import reply_id
