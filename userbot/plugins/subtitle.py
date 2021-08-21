28ad59a901d66fbee23bfffe29c0b37f428ecbb0
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-08-21 15:00:25 +0530

    Update subtitle.py

--- userbot/plugins/subtitle.py
+++ userbot/plugins/subtitle.py
@@ -78,3 +78,23 @@ def get_lang(keyword):
 async def subtitle(event):
     "Get Subtitles of Given Movies"
     input_str = event.pattern_match.group(1)
+   def searching(update: Update, context: CallbackContext):
+    if update.message.via_bot != None:
+        return
+
+    search_message = context.bot.send_message(chat_id=update.effective_chat.id, text="Searching your subtitle file")
+    sub_name = update.effective_message.text
+    full_index, title, keyword = search_sub(sub_name)
+    inline_keyboard = []
+    if len(full_index) == 0:
+        context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=search_message.message_id, text="No results found")
+        return
+    
+    index = full_index[:15]
+    for i in index:
+        subtitle = title[i-1]
+        key = keyword[i-1]
+        inline_keyboard.append([InlineKeyboardButton(subtitle, callback_data=f"{key}")])
+
+    reply_markup = InlineKeyboardMarkup(inline_keyboard)
+    context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=search_message.message_id, text=f"Got the following results for your query *{sub_name}*. Select the preffered type from the below options", parse_mode="Markdown", reply_markup=reply_markup)
